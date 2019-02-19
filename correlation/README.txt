84932ea1f24537beddfdb34d60658f8d3b0e35cc
maheeka <maheeka.j@gmail.com>
2019-02-19 19:00:10 +1100

    Add basic spring boot application calling weather API

---
+++ correlation/README.txt
@@ -0,0 +1,6 @@
+1. Start the application with
+    mvn spring-boot:run
+
+2. Invoke the service's get today's weather API with
+        $ curl http://localhost:9000/today?city=Sydney
+          Weather today is broken clouds
---
+++ correlation/src/main/java/com/springexp/correlation/controller/WeatherController.java
@@ -0,0 +1,43 @@
+package com.springexp.correlation.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+public class WeatherController {
+
+    Logger log = LoggerFactory.getLogger(WeatherController.class);
+
+    @Value("${api.weather.host}")
+    String weatherAPIHost;
+
+    @Value("${api.weather.appid}")
+    String weatherAPIAppId;
+
+    @RequestMapping("/today")
+    public String getWeatherToday(@RequestParam(value = "city") String city) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String getWeatherByCityUrl = weatherAPIHost + "?q={city}&appid={appId}";
+
+        ResponseEntity<Map> response = restTemplate.getForEntity(getWeatherByCityUrl, Map.class, city, weatherAPIAppId);
+
+        log.info("Weather in {} response : {} ", city, response.getBody());
+
+        String weatherDescription = (String) ((Map) ((List) response.getBody().get("weather")).get(0)).get("description");
+
+        return "Weather today is " + weatherDescription + "\n";
+    }
+
+}
+
